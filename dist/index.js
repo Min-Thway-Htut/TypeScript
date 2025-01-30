8891936dceae512eb73976799970315907742c83
Min-Thway-Htut <minthwayhtut568@gmail.com>
2025-01-30 21:58:10 +0100

    Second Update

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+var age = 20;
+if (age < 50)
+    age += 10;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+level = 1;
+level = "a";
+
+function render(document: any) {
+    console.log(document);
+}
+
