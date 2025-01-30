bef7b11746b5582190a2f46eb858e9ead60fe7cc
Min-Thway-Htut <minthwayhtut568@gmail.com>
2025-01-30 23:28:05 +0100

    Another Update

--- src/ts.ts
+++ src/ts.ts
@@ -16,3 +16,20 @@ let textBox: UIWidget = {
 type Quantity = 50 | 100;
 let quantity: 50 | 100 = 50;
 
+function greet(name: string) {
+    if(name)
+    console.log(name.toUpperCase());
+    else
+    console.log("Hola");
+}
+
+console.log(null);
+
+
+type Customer = {
+    birthday: Date;
+}
+
+function getCustomer(id: number): Customer | null {
+    return id === 0 ? null : {birthday: new Date()};
+}
