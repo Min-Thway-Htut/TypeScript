74d1cd4d5460709983039dedd17fe811bef8fbe4
Min-Thway-Htut <minthwayhtut568@gmail.com>
2025-01-30 22:47:41 +0100

    Another Update

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,21 @@
 "use strict";
-var age = 20;
-if (age < 50)
-    age += 10;
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+level = 1;
+level = "a";
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+let user = [1, "John"];
+var Size;
+(function (Size) {
+    Size["Small"] = "s";
+    Size["Medium"] = "m";
+    Size["Large"] = "l";
+})(Size || (Size = {}));
+;
+let mySize = Size.Medium;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,31 @@ function render(document: any) {
     console.log(document);
 }
 
+let numbers: number[] = [1,2,3];
+
+
+let user: [number, string] = [1, "John"];
+
+
+
+
+enum Size {Small = 's', Medium = 'm', Large = 'l'};
+let mySize: Size = Size.Medium;
+
+function calculateTax(income: number, taxTear = 2022): number{
+    if (taxTear < 50000) {
+        return income * 1.2;
+    }else {
+        return income * 1.4;
+    }
+}
+
+
+let employee: {
+    id: number,
+    name: string,
+    retire: (date: Date) => void
+} = {id: 1, name: "John", retire: (date: Date) => 
+    console.log(date)
+};
+employee.name = "Mosh";
